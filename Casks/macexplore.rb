c4ed0ae1747d619f23289d66cf97f04452b8e332
Osift <dev@Osifts-MacBook-Pro.local>
2026-01-18 10:27:53 -0800

    cask

---
+++ Casks/macexplore.rb
@@ -0,0 +1,11 @@
+cask "macexplore" do
+  version "1.10"
+  sha256 "36c1f59de809d7f870109a4d2db0698c52e0f81d79b33e85c326ba10a584ad00"
+  
+  url "https://github.com/osift/macexplore/releases/download/v#{version}/MacExplore-#{version}.dmg"
+  name "MacExplore"
+  desc "Modern file explorer for macOS"
+  homepage "https://macexplore.com"
+  
+  app "MacExplore.app"
+end
