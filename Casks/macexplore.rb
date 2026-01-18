9cb120cb1c417f4b90e13ce6025b5dee31b2c8bc
Osift <dev@Osifts-MacBook-Pro.local>
2026-01-18 11:32:04 -0800

    cask

--- Casks/macexplore.rb
+++ Casks/macexplore.rb
@@ -1,6 +1,6 @@
 cask "macexplore" do
-  version "1.10"
-  sha256 "36c1f59de809d7f870109a4d2db0698c52e0f81d79b33e85c326ba10a584ad00"
+  version "1.11"
+  sha256 "153a02e1961950a0b2ff79c02b20a8c17e728a735f339f6698e919e7fb86b469"
 
   url "https://github.com/osift/macexplore/releases/download/v#{version}/MacExplore-#{version}.dmg",
       verified: "github.com/osift/macexplore/"
